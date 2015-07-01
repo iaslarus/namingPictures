25bf5157bf2b529e18ab35f07c7a0cc2fee389db
iaslarus <fizzy1999@gmail.com>
2015-07-01 10:50:24 -0400

    1st commit

---
+++ namingPictures/ImageView.swift
@@ -0,0 +1,16 @@
+//
+//  ImageView.swift
+//  namingPictures
+//
+//  Created by School on 7/1/15.
+//  Copyright (c) 2015 sunspot. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+class ImageView: UIView{
+    
+}
--- namingPictures/ViewController.swift
+++ namingPictures/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let imageViewFrame = CGRect(x: 0.0, y: 60.0, width: view.bounds.width, height: view.bounds.height-50)
+        
+        imageView = ImageView(frame: imageViewFrame)
+        
+        println("\(view.bounds.width) \(view.bounds.height)")
+        
+        view.addSubview(imageView)
+        
+        imageView.reset()
+    
     }
 
     override func didReceiveMemoryWarning() {
